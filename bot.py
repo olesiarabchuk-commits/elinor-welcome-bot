8fc802e8c75d622c0327286346c201140afa1d62
olesiarabchuk-commits <olesiarabchuk@gmail.com>
2026-06-11 14:48:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,8 @@
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
-from flask import Flask
 import os
-import threading
 
 TOKEN = os.getenv("BOT_TOKEN")
-
-web = Flask(__name__)
-
-@web.route("/")
-def home():
-    return "Bot is running"
 
 async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message and update.message.new_chat_members:
@@ -20,13 +12,10 @@ async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 f"Раді бачити вас у спільноті майстрів Elinor 💜"
             )
 
-def run_bot():
-    app = Application.builder().token(TOKEN).build()
-    app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
-    app.run_polling()
+app = Application.builder().token(TOKEN).build()
 
-threading.Thread(target=run_bot).start()
+app.add_handler(
+    MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome)
+)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    web.run(host="0.0.0.0", port=port)
+app.run_polling()
